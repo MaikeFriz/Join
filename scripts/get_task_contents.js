0a0df646f15afb5908e4af1f84ad9cf5ef92e1e9
MarcelSoerensen <marcel.soerensen@icloud.com>
2025-03-15 18:54:16 +0100

    add assignees to tasks and create more files

---
+++ scripts/get_task_contents.js
@@ -0,0 +1,91 @@
+
+function addHTMLToTaskContainers(todoCardsHTML, inProgressCardsHTML, awaitingFeedbackCardsHTML, doneCardsHTML)
+{
+    let todoCardContainer = document.getElementById('toDoCard');
+    let inProgressCardContainer = document.getElementById('inProgressCard');
+    let awaitingFeedbackContainer = document.getElementById('awaitFeedbackCard');
+    let doneCardContainer = document.getElementById('doneCard');
+
+    todoCardContainer.innerHTML += todoCardsHTML;
+    inProgressCardContainer.innerHTML += inProgressCardsHTML;
+    awaitingFeedbackContainer.innerHTML += awaitingFeedbackCardsHTML;
+    doneCardContainer.innerHTML += doneCardsHTML;
+}
+
+function getTodoContent(userDataContent) {
+    let todoCardsHTML = '';
+    if (!userDataContent.assignedTasks || !userDataContent.assignedTasks.todos || Object.keys(userDataContent.assignedTasks.todos).length === 0) {
+        console.log("Todo: Keine Aufgaben für diesen User");
+    } else {
+        let todos = userDataContent.assignedTasks.todos;
+        let todosArray = Object.values(todos);
+        for (let todoIndex = 0; todoIndex < todosArray.length; todoIndex++) {
+            let todoContent = todosArray[todoIndex];
+            console.log("Todo:", todoContent);
+            todoCardsHTML += todoCardTemplate(todoContent);
+        }
+    }
+    return todoCardsHTML;
+}
+
+function getInProgressContent(userDataContent) {
+    let inProgressCardsHTML = '';
+    if (!userDataContent.assignedTasks || !userDataContent.assignedTasks.inProgress || Object.keys(userDataContent.assignedTasks.inProgress).length === 0) {
+        console.log("In Progress: Keine Aufgaben für diesen User");
+    } else {
+        let inProgress = userDataContent.assignedTasks.inProgress;
+        let inProgressArray = Object.values(inProgress);
+        for (let inProgressIndex = 0; inProgressIndex < inProgressArray.length; inProgressIndex++) {
+            let inProgressContent = inProgressArray[inProgressIndex];
+            console.log("In Progress:", inProgressContent);
+            inProgressCardsHTML += inProgressCardTemplate(inProgressContent);
+        }
+    }
+    return inProgressCardsHTML;
+}
+
+function getAwaitingFeedbackContent(userDataContent) {
+    let awaitingFeedbackCardsHTML = '';
+    if (!userDataContent.assignedTasks || !userDataContent.assignedTasks.awaitingFeedback || Object.keys(userDataContent.assignedTasks.awaitingFeedback).length === 0) {
+        console.log("Awaiting Feedback: Keine Aufgaben für diesen User");
+    } else {
+        let awaitingFeedback = userDataContent.assignedTasks.awaitingFeedback;
+        let awaitingFeedbackArray = Object.values(awaitingFeedback);
+        for (let awaitingFeedbackIndex = 0; awaitingFeedbackIndex < awaitingFeedbackArray.length; awaitingFeedbackIndex++) {
+            let awaitingFeedbackContent = awaitingFeedbackArray[awaitingFeedbackIndex];
+            console.log("Awaiting Feedback:", awaitingFeedbackContent);
+            awaitingFeedbackCardsHTML += awaitingFeedbackCardTemplate(awaitingFeedbackContent);
+        }
+    }
+    return awaitingFeedbackCardsHTML;
+}
+
+function getDoneContent(userDataContent) {
+    let doneCardsHTML = '';
+    if (!userDataContent.assignedTasks || !userDataContent.assignedTasks.done || Object.keys(userDataContent.assignedTasks.done).length === 0) {
+        console.log("Done: Keine Aufgaben für diesen User");
+    } else {
+        let done = userDataContent.assignedTasks.done;
+        let doneArray = Object.values(done);
+        for (let doneIndex = 0; doneIndex < doneArray.length; doneIndex++) {
+            let doneContent = doneArray[doneIndex];
+            console.log("Done:", doneContent);
+            doneCardsHTML += doneCardTemplate(doneContent);
+        }
+    }
+    return doneCardsHTML;
+}
+
+function getAssignees(taskContent) {
+    let assigneesHTML = '';
+    if (taskContent.assignees && taskContent.assignees.length > 0) {
+        for (let assigneesIndex = 0; assigneesIndex < taskContent.assignees.length; assigneesIndex++) {
+            const taskAssignee = taskContent.assignees[assigneesIndex];
+            console.log("Mitarbeiter:", taskAssignee);
+            assigneesHTML += assigneeTemplate(taskAssignee);
+        }
+    } else {
+        assigneesHTML = "<span class='no-assignees'>Keine Mitarbeiter zugewiesen</span>";
+    }
+    return assigneesHTML;
+}
---
+++ scripts/task_card_templates.js
@@ -0,0 +1,58 @@
+
+function assigneeTemplate(taskAssignee) {
+    return /*html*/`
+    <span class="assignee">${taskAssignee}</span>
+    `;
+}
+
+function todoCardTemplate(todoContent) {
+    return /*html*/`
+    <div class="board-task">
+        <label class="${todoContent.label.split(' ').join('-').toLowerCase()}">${todoContent.label}</label>
+        <h3>${todoContent.title || "Unbenannte Aufgabe"}</h3>
+        <p>${todoContent.description || "Keine Beschreibung"}</p>
+        <div class="assignees-container">
+            ${getAssignees(todoContent)}
+        </div>
+    </div>
+    `;
+}
+
+function inProgressCardTemplate(inProgressContent) {
+    return /*html*/`
+    <div class="board-task">
+        <label class="${inProgressContent.label.split(' ').join('-').toLowerCase()}">${inProgressContent.label}</label>
+        <h3>${inProgressContent.title || "Unbenannte Aufgabe"}</h3>
+        <p>${inProgressContent.description || "Keine Beschreibung"}</p>
+        <div class="assignees-container">
+            ${getAssignees(inProgressContent)}
+        </div>
+    </div>
+    `;
+}
+
+function awaitingFeedbackCardTemplate(awaitingFeedbackContent) {
+    return /*html*/`
+    <div class="board-task">
+        <label class="${awaitingFeedbackContent.label.split(' ').join('-').toLowerCase()}">${awaitingFeedbackContent.label}</label>
+        <h3>${awaitingFeedbackContent.title || "Unbenannte Aufgabe"}</h3>
+        <p>${awaitingFeedbackContent.description || "Keine Beschreibung"}</p>
+        <div class="assignees-container">
+            ${getAssignees(awaitingFeedbackContent)}
+        </div>
+    </div>
+    `;
+}
+
+function doneCardTemplate(doneContent) {
+    return /*html*/`
+    <div class="board-task">
+        <label class="${doneContent.label.split(' ').join('-').toLowerCase()}">${doneContent.label}</label>
+        <h3>${doneContent.title || "Unbenannte Aufgabe"}</h3>
+        <p>${doneContent.description || "Keine Beschreibung"}</p>
+        <div class="assignees-container">
+            ${getAssignees(doneContent)}
+        </div>
+    </div>
+    `;
+}
