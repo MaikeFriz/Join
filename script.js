92ad54db437a5a4adfa2c3b21fdf6c4197b1f45b
Uronese <urosnedeljkovic44@gmail.com>
2025-03-16 11:33:09 +0100

    refactor: improve code readability by formatting and aligning fetchKanbanData function

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-const BASE_URL = "https://join-36b1f-default-rtdb.europe-west1.firebasedatabase.app/users.json";
+const BASE_URL =
+  "https://join-36b1f-default-rtdb.europe-west1.firebasedatabase.app/users.json";
 
 Promise.resolve().then(async () => {
-    await fetchKanbanData();
+  await fetchKanbanData();
 });
 
 async function fetchKanbanData() {
-    let response = await fetch(BASE_URL);
-    let kanbanData = await response.json();
-    getDataContent(kanbanData);
+  let response = await fetch(BASE_URL);
+  let kanbanData = await response.json();
+  getDataContent(kanbanData);
 }
 
 function getDataContent(kanbanData) {
-    let dataArray = Object.values(kanbanData);
-    console.log(dataArray); 
-    for (let dataIndex = 0; dataIndex < dataArray.length; dataIndex++) {
-        let userDataContent = dataArray[dataIndex];
-        console.log(`User: ${userDataContent.name}`);
-        
-        let todoCardsHTML = getTodoContent(userDataContent);
-        let inProgressCardsHTML = getInProgressContent(userDataContent);
-        let awaitingFeedbackCardsHTML = getAwaitingFeedbackContent(userDataContent);
-        let doneCardsHTML = getDoneContent(userDataContent);
+  let dataArray = Object.values(kanbanData);
+  console.log(dataArray);
+  for (let dataIndex = 0; dataIndex < dataArray.length; dataIndex++) {
+    let userDataContent = dataArray[dataIndex];
+    console.log(`User: ${userDataContent.name}`);
 
-        addHTMLToTaskContainers(todoCardsHTML, inProgressCardsHTML, awaitingFeedbackCardsHTML, doneCardsHTML);
-    }
+    let todoCardsHTML = getTodoContent(userDataContent);
+    let inProgressCardsHTML = getInProgressContent(userDataContent);
+    let awaitingFeedbackCardsHTML = getAwaitingFeedbackContent(userDataContent);
+    let doneCardsHTML = getDoneContent(userDataContent);
+
+    addHTMLToTaskContainers(
+      todoCardsHTML,
+      inProgressCardsHTML,
+      awaitingFeedbackCardsHTML,
+      doneCardsHTML
+    );
+  }
 }
-
-
