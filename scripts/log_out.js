797f46e65c55e64cf08d2280db66f1c508ffdd4b
Thierno Diallo <thiernomam78@gmail.com>
2025-05-14 02:22:26 +0200

    logout wurde geändert

--- scripts/log_out.js
+++ scripts/log_out.js
@@ -1,16 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const checkLogoutButton = setInterval(() => {
-    const logoutButton = document.getElementById("logoutButton");
-    if (logoutButton) {
-      logoutButton.addEventListener("click", logoutUser);
-      clearInterval(checkLogoutButton);
-    }
-  }, 100);
-});
-
+// Logout-Funktion
 function logoutUser() {
   localStorage.removeItem("loggedInUser");
   localStorage.removeItem("isGuest");
   localStorage.removeItem("guestKanbanData");
   window.location.href = "./index.html";
 }
+
+// Event Delegation für dynamische Elemente
+document.body.addEventListener("click", function (event) {
+  if (event.target.closest("#logoutButton")) {
+    logoutUser();
+  }
+});
